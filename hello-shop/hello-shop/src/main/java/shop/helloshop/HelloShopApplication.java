e7b39f8e30271eb19a5b609b6beadba3caf1b92a
blackdooyu <84924161+blackdooyu@users.noreply.github.com>
2021-12-05 23:07:11 +0900

    start

---
+++ hello-shop/hello-shop/src/main/java/shop/helloshop/HelloShopApplication.java
@@ -0,0 +1,13 @@
+package shop.helloshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloShopApplication.class, args);
+	}
+
+}
